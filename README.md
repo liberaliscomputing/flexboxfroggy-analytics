bdb885a8f7d691919d362a6028cf84659a182ff6
liberaliscomputing <liberalis.computing@gmail.com>
2016-06-13 21:18:59 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Code for Analyzing Flexbox Froggy (http://www.flexboxfroggy.com/) Data 
---
+++ merge_data.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os
+import simplejson as json
+
+def merge_data(dname):
+	data = {}
+	for fname in os.listdir(dname):
+		path = dname + fname
+		imported_json = json.loads(open(path).read())
+		data.update(imported_json)
+
+	with open(dname + 'data.json', 'w') as f:
+		f.write(json.dumps(data, indent=2))
+
+if __name__ == '__main__':
+	dname = 'data/'
+	merge_data(dname)
